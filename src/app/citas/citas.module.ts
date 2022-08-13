292e9bd2aaf792f5213be46e7a1af21ddff1ce89
arojasvargas <alex.rojas547@outlook.com>
2022-08-13 00:29:13 -0600

    modulo de citas

---
+++ src/app/citas/citas.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from "@angular/core";
+import { CitasComponent } from "./citas/citas.component";
+
+@NgModule({
+    declarations: [ CitasComponent ],
+    exports: [ CitasComponent ],
+})
+
+export class CitasModule { }
---
+++ src/app/citas/citas/citas.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-citas',
+  templateUrl: './citas.component.html',
+  styleUrls: ['./citas.component.css']
+})
+
+export class CitasComponent { }
+
+
+// <!-- -->
+// export class CitasComponent implements OnInit {
+//   constructor() { }
+//   ngOnInit(): void {
+//   }
+// }
